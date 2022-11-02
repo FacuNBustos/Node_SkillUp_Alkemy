3bcd8c2aa8b6860a875a8c0d9ebf969d92e5e6b8
Lucas Figueroa <lucas.200061@gmail.com>
2022-11-02 11:10:39 -0300

    generate model&migration transaction

---
+++ database/models/transaction.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class transaction extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  transaction.init({
+    description: DataTypes.STRING,
+    amount: DataTypes.DECIMAL,
+    date: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'transaction',
+  });
+  return transaction;
+};
