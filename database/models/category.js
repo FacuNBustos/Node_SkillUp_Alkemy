b0d13f87f6c0a6364acfe8e3c75a94e34d78666f
Facundo <Facu.Bustos00@hotmail.com>
2022-11-02 02:24:31 -0300

    remove comments of sequelize model

--- database/models/category.js
+++ database/models/category.js
@@ -4,14 +4,6 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class category extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
   };
   category.init({
     name: DataTypes.STRING,
