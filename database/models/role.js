38bb4c4acb518227a0983d70e9423585b2b73e17
Cristhian D'Alessandro <crist.dalessandro@gmail.com>
2022-11-02 14:31:35 -0300

    [BD-003] Se agrega asociación entre role y usuario.

--- database/models/role.js
+++ database/models/role.js
@@ -3,7 +3,11 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class role extends Model {};
+  class role extends Model {
+    static associate(models) {
+      role.hasMany(models.user)
+    }
+  };
   role.init({
     name: DataTypes.STRING,
     description: DataTypes.STRING
