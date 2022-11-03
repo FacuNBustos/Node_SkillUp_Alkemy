533b9784075d6459dcb0756af588d8803a58b395
unknown <agustinf_tw@hotmail.com>
2022-11-03 19:33:51 -0300

    create users controller & schema

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,13 @@ const express = require('express')
 const schemaValidator = require('../middlewares/schemaValidator');
 const deleteUser = require('../controllers/users/delete.user');
 const deleteSchema = require('../schemas/users/delete.schema');
+const createSchema = require('../schemas/users/create.schema');
 const { getAllUsers }  = require('../controllers/users/getAllUser');
+const { createUsers }  = require('../controllers/users/create.user');
 
 const router = express.Router();
 router.get('/', getAllUsers);
-
+router.post('/', schemaValidator(createSchema), createUsers);
 router.delete('/:id', schemaValidator(deleteSchema), deleteUser.run);
 
 
