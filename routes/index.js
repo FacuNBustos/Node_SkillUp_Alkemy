65a0fc55a49524f191f03373fc2fb4cd049f7388
unknown <agustinf_tw@hotmail.com>
2022-11-03 16:04:17 -0300

    commit before push

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { getAllUsers } = require('../controllers/users/getAllUsers');
+
 const router = express.Router();
 
 
-router.get('/users', getAllUsers);
+
 
 module.exports = router
--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,12 @@ const express = require('express')
 const schemaValidator = require('../middlewares/schemaValidator');
 const deleteUser = require('../controllers/users/delete.user');
 const deleteSchema = require('../schemas/users/delete.schema');
+const { getAllUsers }  = require('../controllers/users/getAllUser');
 
 const router = express.Router();
+router.get('/', getAllUsers);
 
 router.delete('/:id', schemaValidator(deleteSchema), deleteUser.run);
+
 
 module.exports = router
