f8ea2e400dd881408d81c676be8ccbd4d7ead619
unknown <agustinf_tw@hotmail.com>
2022-11-03 19:40:07 -0300

    fixes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
-const express = require('express');
 
-const router = express.Router();
+const express = require('express')
+const { get } = require('../controllers/index')
 
+const router = express.Router()
 
-
+// example of a route with index controller get function
+router.get('/', get)
 
 module.exports = router
