6c4126b9445c3e2e09c03580507845d6a8fd2506
unknown <agustinf_tw@hotmail.com>
2022-11-03 19:47:19 -0300

    fixes EU-007

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,24 @@
-const express = require('express');
+const createHttpError = require('http-errors')
+const { Test } = require('../database/models')
+const { endpointResponse } = require('../helpers/success')
+const { catchAsync } = require('../helpers/catchAsync')
 
-const router = express.Router();
-
-
-
-
-module.exports = router
+// example of a controller. First call the service, then build the controller method
+module.exports = {
+  get: catchAsync(async (req, res, next) => {
+    try {
+      const response = await Test.findAll()
+      endpointResponse({
+        res,
+        message: 'Test retrieved successfully',
+        body: response,
+      })
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error retrieving index] - [index - GET]: ${error.message}`,
+      )
+      next(httpError)
+    }
+  }),
+}
