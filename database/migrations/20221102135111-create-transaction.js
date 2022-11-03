616d692102f9d4369c55f21b1d7815519ac9c151
Lucas Figueroa <71677198+lucas22-f@users.noreply.github.com>
2022-11-03 10:36:50 -0300

    [BD-005] Crear modelo y migración de Transaction (#6)
    * generate model&migration transaction
    
    * associate & model config
    
    * add deleteAt column

---
+++ database/migrations/20221102135111-create-transaction.js
@@ -0,0 +1,56 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('transactions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      amount: {
+        type: Sequelize.DECIMAL,
+        allowNull: false
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      },
+      userId:{
+        type: Sequelize.INTEGER,
+         references:{
+          model:'users',
+          key:'id'
+        } 
+      },
+      categoryId:{
+        type: Sequelize.INTEGER,
+         references:{
+          model:'categories',
+          key:'id'
+        } 
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+      },
+      deletedAt: {
+        type:Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('transactions');
+  }
+};
