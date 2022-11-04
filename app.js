0f6e034484e039cce961b1920b8841c75e0fd45a
Lucas Figueroa <lucas.200061@gmail.com>
2022-11-04 00:12:27 -0300

    ET-012 getAll Transactions

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 require('dotenv').config()
 
 const indexRouter = require('./routes/index')
-
+const transactionRouter = require("./routes/transaction")
 const port = process.env.PORT || 3000
 
 const app = express()
@@ -20,6 +20,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
+app.use('/transactions',transactionRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
