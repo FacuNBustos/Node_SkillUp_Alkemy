43ece820e1983db98c57ea0f543c72fe01153225
unknown <agustinf_tw@hotmail.com>
2022-11-04 11:44:32 -0300

    EU-007

--- controllers/index.js
+++ controllers/index.js
@@ -1,20 +1,13 @@
 const createHttpError = require('http-errors')
-const  { user }  = require('../database/models')
+const { Test } = require('../database/models')
 const { endpointResponse } = require('../helpers/success')
 const { catchAsync } = require('../helpers/catchAsync')
 
-
-// const UserModel = models.Test;
-// async function  hola  = () => {
-// console.log( await Test)
-// } 
 // example of a controller. First call the service, then build the controller method
 module.exports = {
- 
   get: catchAsync(async (req, res, next) => {
-
     try {
-      const response = await user.findAll()
+      const response = await Test.findAll()
       endpointResponse({
         res,
         message: 'Test retrieved successfully',
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,9 @@
-const createHttpError = require('http-errors')
-const { Test } = require('../database/models')
-const { endpointResponse } = require('../helpers/success')
-const { catchAsync } = require('../helpers/catchAsync')
+const express = require('express')
+const { get } = require('../controllers/index')
 
-// example of a controller. First call the service, then build the controller method
-module.exports = {
-  get: catchAsync(async (req, res, next) => {
-    try {
-      const response = await Test.findAll()
-      endpointResponse({
-        res,
-        message: 'Test retrieved successfully',
-        body: response,
-      })
-    } catch (error) {
-      const httpError = createHttpError(
-        error.statusCode,
-        `[Error retrieving index] - [index - GET]: ${error.message}`,
-      )
-      next(httpError)
-    }
-  }),
-}
+const router = express.Router()
+
+// example of a route with index controller get function
+router.get('/', get)
+
+module.exports = router
