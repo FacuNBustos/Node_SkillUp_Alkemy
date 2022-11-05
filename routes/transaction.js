7760614a98a5deee930c40ec8e4f830a53fd5a2d
Agustinf90 <88116635+Agustinf90@users.noreply.github.com>
2022-11-05 16:56:15 -0300

    [ET-016] Endpoint para editar Transacciones (#22)
    * update transaction controller

--- routes/transaction.js
+++ routes/transaction.js
@@ -4,11 +4,15 @@ const {getById} = require('../controllers/transactions/getById.transaction');
 const {get} = require("../controllers/transactions/getAll.transactions");
 const {deleteOne} = require('../controllers/transactions/delete.transaction');
 const idSchema = require('../schemas/transactions/getById.schema');
+const updateSchema = require('../schemas/transactions/update.schema')
+const { updateById } = require('../controllers/transactions/updateById.transaction')
 const deleteSchema = require('../schemas/transactions/delete.schema')
+
 const router = express.Router();
 
 router.get("/", get)
 router.get('/:id', schemaValidator(idSchema), getById);
+router.post('/:id', schemaValidator(updateSchema), updateById);
 router.delete('/:id', schemaValidator(deleteSchema), deleteOne)
 
 module.exports = router;
