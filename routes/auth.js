af68d8616b00260a2c6f4881e763cb2a5c89e5a1
Adrián González <106711444+adriangonzalez-dev@users.noreply.github.com>
2022-11-05 15:29:42 -0300

    [AL-023] Endpoint para autenticar Users (#19)
    * AL-023 login controller added
    
    * AL-023 Endpoint para autenticar Users

---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const schemaValidator = require('../middlewares/schemaValidator');
+const {login} = require('../controllers/auth/login.auth');
+const loginSchema = require('../schemas/auth/login.schema')
+
+const router = express.Router();
+
+router.post('/login', schemaValidator(loginSchema), login);
+
+
+module.exports = router;
