9c6c2ca119efb37751663bbbe0700ccab35d11a8
Facundo <Facu.Bustos00@hotmail.com>
2022-11-05 13:25:43 -0300

    add update category feachure

--- routes/category.js
+++ routes/category.js
@@ -2,9 +2,12 @@ const express = require('express');
 const schemaValidator = require('../middlewares/schemaValidator');
 const createSchema = require('../schemas/categories/create.schema');
 const createCategory = require('../controllers/categories/create.category');
+const getByIdSchema = require("../schemas/categories/getById.schema");
+const getByIdCategory = require('../controllers/categories/getById.category');
 
 const router = express.Router();
 
 router.post('', schemaValidator(createSchema), createCategory.run);
+router.put("/:id", schemaValidator(getByIdSchema), getByIdCategory.run)
 
 module.exports = router;
