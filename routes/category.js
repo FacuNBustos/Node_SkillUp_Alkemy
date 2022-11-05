766b721bda547661b132ef638b63c1edb4cb8859
Cristhian D'Alessandro <97862610+cristdales@users.noreply.github.com>
2022-11-05 13:55:34 -0300

    [EC-018] Endpoint para listar categorías (#20)

--- routes/category.js
+++ routes/category.js
@@ -2,9 +2,11 @@ const express = require('express');
 const schemaValidator = require('../middlewares/schemaValidator');
 const createSchema = require('../schemas/categories/create.schema');
 const createCategory = require('../controllers/categories/create.category');
+const getAllCategories = require('../controllers/categories/getAll.category');
 
 const router = express.Router();
 
 router.post('', schemaValidator(createSchema), createCategory.run);
+router.get('/', getAllCategories.run);
 
 module.exports = router;
