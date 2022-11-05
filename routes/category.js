31733ff8960732dba800614684da1688ff55c3b7
Facundo Bustos Goainochea <82781094+FacuNBustos@users.noreply.github.com>
2022-11-05 17:39:46 -0300

    [ EC-019 ] Endpoint para datos de Categorías (#23)
    * add update category feachure
    
    * change verb http of get by id category route

--- routes/category.js
+++ routes/category.js
@@ -2,10 +2,13 @@ const express = require('express');
 const schemaValidator = require('../middlewares/schemaValidator');
 const createSchema = require('../schemas/categories/create.schema');
 const createCategory = require('../controllers/categories/create.category');
+const getByIdSchema = require("../schemas/categories/getById.schema");
+const getByIdCategory = require('../controllers/categories/getById.category');
 const getAllCategories = require('../controllers/categories/getAll.category');
 
 const router = express.Router();
 
+router.get("/:id", schemaValidator(getByIdSchema), getByIdCategory.run)
 router.post('', schemaValidator(createSchema), createCategory.run);
 router.get('/', getAllCategories.run);
 
