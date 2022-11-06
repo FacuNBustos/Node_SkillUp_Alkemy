f12f581b1128dc8529b5146be88685deed32b0f0
khuaza9612 <andresnavia-96@hotmail.com>
2022-11-05 19:29:03 -0500

    Update create.transaction.js

--- controllers/transactions/create.transaction.js
+++ controllers/transactions/create.transaction.js
@@ -3,12 +3,29 @@ const { transaction } = require('../../database/models')
 const { endpointResponse } = require('../../helpers/success')
 const { catchAsync } = require('../../helpers/catchAsync')
 const { ErrorObject } = require('../../helpers/error')
+const { user } = require("../../database/models");
+const { category } = require("../../database/models");
 
 // example of a controller. First call the service, then build the controller method
 module.exports = {
    post: catchAsync(async (req, res, next) => {
           try {
-        
+         const users=await user.findOne({
+            where: {
+              id: req.body.userId},
+            });
+            console.log(users)
+            if (!users) {
+              throw new ErrorObject("This user already not exists", 404);
+          };
+          const categories=await category.findOne({
+            where: {
+              id: req.body.categoryId},
+            });
+            if (!categories) {
+              throw new ErrorObject("This category already not exists", 404);
+          };
+
         const { description, amount, date, userId, categoryId } = req.body
         const newTransaction = await transaction.create({ description, amount, date, userId, categoryId })
         if (!newTransaction) {
