78fcd30b5436860663222ea53847b75281220993
Cristhian D'Alessandro <crist.dalessandro@gmail.com>
2022-11-07 13:52:23 -0300

    Se corrige el schema para update de categorías

--- schemas/categories/update.schema.js
+++ schemas/categories/update.schema.js
@@ -1,16 +1,20 @@
 const updateSchema = {
-    name: {
-      in: ['body'],
-      optional: false,
-      notEmpty: false,
-      
-    },
-    description: {
-      in: ['body'],
-      optional: false,
-      notEmpty: false,
-      
-    },
-  };
-  
-  module.exports = updateSchema;
+  id: {
+    in: ['params'],
+    isInt: true,
+    toInt: true,
+    errorMessage: 'ID is wrong',
+  },
+  name: {
+    in: ['body'],
+    optional: true,
+    notEmpty: true,
+    errorMessage: 'category name cannot be empty',
+  },
+  description: {
+    in: ['body'],
+    optional: true,
+  },
+};
+
+module.exports = updateSchema;
