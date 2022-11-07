50a2c56d6f9b95a54af0caa2abbe441f759ab3b0
Facundo <Facu.Bustos00@hotmail.com>
2022-11-07 13:10:32 -0300

    remove call method in user routes

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,6 @@ const deleteSchema = require('../schemas/users/delete.schema');
 const createSchema = require('../schemas/users/create.schema');
 const { getAllUsers }  = require('../controllers/users/getAllUser');
 const { createUsers }  = require('../controllers/users/create.user');
-const { getAllUsers }  = require('../controllers/users/getAllUser');
 const idSchema = require('../schemas/users/getid.schema');
 const updateSchema = require('../schemas/users/update.schema');
 const updateUser = require('../controllers/users/update.user');
