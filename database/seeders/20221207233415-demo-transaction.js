b3ba6c744a4024025acd5c316e8b7245144f6d07
Lucas Figueroa <lucas.200061@gmail.com>
2022-11-08 16:08:20 -0300

    add id and category

--- database/seeders/20221207233415-demo-transaction.js
+++ database/seeders/20221207233415-demo-transaction.js
@@ -5,72 +5,29 @@ module.exports = {
     return await queryInterface.bulkInsert(
       'transactions',
       [
-        {
-          description: 'transaccion efectuada por usuario',
-          amount: 1350,
-          userId: 1,
-          categoryId: 1,
-        },
-        {
-          description: 'transaccion efectuada por usuario',
-          amount: 2500,
-          userId: 2,
-          categoryId: 2,
-        },
-        {
-          description: 'transaccion efectuada por usuario',
-          amount: 5000,
-          userId: 3,
-          categoryId: 3,
-        },
-        {
-          description: 'transaccion efectuada por usuario',
-          amount: 4300,
-          userId: 4,
-          categoryId: 4,
-        },
-        {
-          description: 'transaccion efectuada por usuario',
-          amount: 5356,
-          userId: 5,
-          categoryId: 5,
-        },
-        {
-          description: 'transaccion efectuada por usuario',
-          amount: 6123,
-          userId: 6,
-          categoryId: 6,
-        },
-        {
-          description: 'transaccion efectuada por usuario',
-          amount: 7359,
-          userId: 7,
-          categoryId: 7,
-        },
-        {
-          description: 'transaccion efectuada por usuario',
-          amount: 8600,
-          userId: 8,
-          categoryId: 8,
-        },
-        {
-          description: 'transaccion efectuada por usuario',
-          amount: 9340,
-          userId: 9,
-          categoryId: 9,
-        },
-        {
-          description: 'transaccion efectuada por usuario',
-          amount: 10699,
-          userId: 10,
-          categoryId: 10,
-        },
-      ],
-      {}
-    );
+        { id: 1, description: 'transactions effected ', amount: 1300, userId: 1, categoryId: 1 },
+        { id: 2, description: 'transactions effected ', amount: 1150, userId: 1, categoryId: 2 },
+        { id: 3, description: 'transactions effected ', amount: 2500, userId: 2, categoryId: 1 },
+        { id: 4, description: 'transactions effected ', amount: 2800, userId: 2, categoryId: 2 },
+        { id: 5, description: 'transactions effected ', amount: 3400, userId: 3, categoryId: 1 },
+        { id: 6, description: 'transactions effected ', amount: 3100, userId: 3, categoryId: 2 },
+        { id: 7, description: 'transactions effected ', amount: 4700, userId: 4, categoryId: 1 },
+        { id: 8, description: 'transactions effected ', amount: 4300, userId: 4, categoryId: 2 },
+        { id: 9, description: 'transactions effected ', amount: 5650, userId: 5, categoryId: 1 },
+        { id: 10, description: 'transactions effected ', amount: 5100, userId: 5, categoryId: 2 },
+        { id: 11, description: 'transactions effected ', amount: 6460, userId: 6, categoryId: 1 },
+        { id: 12, description: 'transactions effected ', amount: 6300, userId: 6, categoryId: 2 },
+        { id: 13, description: 'transactions effected ', amount: 7900, userId: 7, categoryId: 1 },
+        { id: 14, description: 'transactions effected ', amount: 7200, userId: 7, categoryId: 2 },
+        { id: 15, description: 'transactions effected ', amount: 8300, userId: 8, categoryId: 1 },
+        { id: 16, description: 'transactions effected ', amount: 8600, userId: 8, categoryId: 2 },
+        { id: 17, description: 'transactions effected ', amount: 9378, userId: 9, categoryId: 1 },
+        { id: 18, description: 'transactions effected ', amount: 9110, userId: 9, categoryId: 2 },
+        { id: 19, description: 'transactions effected ', amount: 10789, userId: 10, categoryId: 1 },
+        { id: 20, description: 'transactions effected ', amount: 10999, userId: 10, categoryId: 2 },
+      ]);
   },
-
   down: async (queryInterface, Sequelize) => {
-    return await queryInterface.bulkDelete('transactions', null, {});
+    return queryInterface.bulkDelete('transactions', null, {});
   },
 };
