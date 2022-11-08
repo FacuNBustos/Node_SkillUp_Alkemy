33310bb0b4340b5a88c2bdd744dde22a5d281a65
adriangonzalez-dev <adriangonzalez.dev90@gmail.com>
2022-11-08 14:31:53 -0300

    EC-027 Endpoint para eliminar Categorías

---
+++ schemas/categories/delete.schema.js
@@ -0,0 +1,10 @@
+const deleteSchema = {
+    id: {
+      in: ['params'],
+      isInt: true,
+      toInt: true,
+      errorMessage: 'ID is wrong',
+    },
+  };
+
+  module.exports = deleteSchema;
