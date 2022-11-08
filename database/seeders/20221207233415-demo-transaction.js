d6f2bde1adf7de6116a597ee92e275d72728d1a2
Lucas Figueroa <lucas.200061@gmail.com>
2022-11-08 13:03:53 -0300

    lint

--- database/seeders/20221207233415-demo-transaction.js
+++ database/seeders/20221207233415-demo-transaction.js
@@ -5,16 +5,66 @@ module.exports = {
     return await queryInterface.bulkInsert(
       'transactions',
       [
-        { description: 'transaccion efectuada por usuario', amount: 1350, userId: 1, categoryId: 1 },
-        { description: 'transaccion efectuada por usuario', amount: 2500, userId: 2, categoryId: 2 },
-        { description: 'transaccion efectuada por usuario', amount: 5000, userId: 3, categoryId: 3 },
-        { description: 'transaccion efectuada por usuario', amount: 4300, userId: 4, categoryId: 4 },
-        { description: 'transaccion efectuada por usuario', amount: 5356, userId: 5, categoryId: 5 },
-        { description: 'transaccion efectuada por usuario', amount: 6123, userId: 6, categoryId: 6 },
-        { description: 'transaccion efectuada por usuario', amount: 7359, userId: 7, categoryId: 7 },
-        { description: 'transaccion efectuada por usuario', amount: 8600, userId: 8, categoryId: 8 },
-        { description: 'transaccion efectuada por usuario', amount: 9340, userId: 9, categoryId: 9 },
-        { description: 'transaccion efectuada por usuario', amount: 10699, userId: 10, categoryId: 10 },
+        {
+          description: 'transaccion efectuada por usuario',
+          amount: 1350,
+          userId: 1,
+          categoryId: 1,
+        },
+        {
+          description: 'transaccion efectuada por usuario',
+          amount: 2500,
+          userId: 2,
+          categoryId: 2,
+        },
+        {
+          description: 'transaccion efectuada por usuario',
+          amount: 5000,
+          userId: 3,
+          categoryId: 3,
+        },
+        {
+          description: 'transaccion efectuada por usuario',
+          amount: 4300,
+          userId: 4,
+          categoryId: 4,
+        },
+        {
+          description: 'transaccion efectuada por usuario',
+          amount: 5356,
+          userId: 5,
+          categoryId: 5,
+        },
+        {
+          description: 'transaccion efectuada por usuario',
+          amount: 6123,
+          userId: 6,
+          categoryId: 6,
+        },
+        {
+          description: 'transaccion efectuada por usuario',
+          amount: 7359,
+          userId: 7,
+          categoryId: 7,
+        },
+        {
+          description: 'transaccion efectuada por usuario',
+          amount: 8600,
+          userId: 8,
+          categoryId: 8,
+        },
+        {
+          description: 'transaccion efectuada por usuario',
+          amount: 9340,
+          userId: 9,
+          categoryId: 9,
+        },
+        {
+          description: 'transaccion efectuada por usuario',
+          amount: 10699,
+          userId: 10,
+          categoryId: 10,
+        },
       ],
       {}
     );
