72de361dc34fb7898c35322d13e4618a0f38e1ea
khuaza9612 <andresnavia-96@hotmail.com>
2022-11-10 14:05:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -54,20 +54,5 @@ app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`Servidor funcionando en el puerto ${port}`);
 });
-/////////////////////////////////////////////////////
-// app.use(multer({
-//   dest: path.join(__dirname, 'public/static'),
-//   fileFilter: function (req, file, cb) {
 
-//       var filetypes = /jpeg|jpg|png|gif/;
-//       var mimetype = filetypes.test(file.mimetype);
-//       var extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-
-//       if (mimetype && extname) {
-//           return cb(null, true);
-//       }
-//       cb("Error: File upload only supports the following filetypes - " + filetypes);
-//   },
-//   limits: {fileSize: 1000000},
-// }).single('image'));
 module.exports = app;
