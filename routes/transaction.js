06fed97093c96b3661da5510aa43717e71a5bd0f
Cristhian D'Alessandro <crist.dalessandro@gmail.com>
2022-11-10 20:06:18 -0300

    implementacion de middlewares: login, ownership, multer

--- routes/transaction.js
+++ routes/transaction.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const schemaValidator = require('../middlewares/schemaValidator');
+const uploadImage = require('../middlewares/multer');
+const { userLogged } = require('../middlewares/userLogged');
+const { ownershipValidator } = require('../middlewares/ownershipValidator');
 const { getById } = require('../controllers/transactions/getById.transaction');
 const { get } = require('../controllers/transactions/getAll.transaction');
 const { deleteOne } = require('../controllers/transactions/delete.transaction');
@@ -14,10 +17,32 @@ const createSchema = require('../schemas/transactions/create.schema');
 
 const router = express.Router();
 
-router.get('/', get);
-router.get('/:id', schemaValidator(idSchema), getById);
-router.post('/', schemaValidator(createSchema), post);
-router.put('/:id', schemaValidator(updateSchema), updateById);
-router.delete('/:id', schemaValidator(deleteSchema), deleteOne);
+router.post('/', schemaValidator(createSchema), userLogged, post);
+
+router.get('/', userLogged, get);
+
+router.get(
+  '/:id',
+  schemaValidator(idSchema),
+  userLogged,
+  ownershipValidator,
+  getById
+);
+
+router.put(
+  '/:id',
+  schemaValidator(updateSchema),
+  userLogged,
+  ownershipValidator,
+  updateById
+);
+
+router.delete(
+  '/:id',
+  schemaValidator(deleteSchema),
+  userLogged,
+  ownershipValidator,
+  deleteOne
+);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const schemaValidator = require('../middlewares/schemaValidator');
+const uploadImage = require('../middlewares/multer');
+const { userLogged } = require('../middlewares/userLogged');
+const { ownershipValidator } = require('../middlewares/ownershipValidator');
 const deleteUser = require('../controllers/users/delete.user');
 const getAllUsersid = require('../controllers/users/getid.user');
 const deleteSchema = require('../schemas/users/delete.schema');
@@ -9,14 +12,41 @@ const { createUsers } = require('../controllers/users/create.user');
 const idSchema = require('../schemas/users/getid.schema');
 const updateSchema = require('../schemas/users/update.schema');
 const updateUser = require('../controllers/users/update.user');
-const uploadImage = require('../middlewares/multer');
 
 const router = express.Router();
 
-router.get('/', getAllUsers);
-router.post('/',schemaValidator(createSchema), createUsers);
-router.delete('/:id', schemaValidator(deleteSchema), deleteUser.run);
-router.get('/:id', schemaValidator(idSchema), getAllUsersid.getid);
-router.put('/:id', schemaValidator(updateSchema), updateUser.run);
+router.post(
+  '/',
+  schemaValidator(createSchema),
+  uploadImage.single('avatar'),
+  createUsers
+);
+
+router.get('/', userLogged, getAllUsers);
+
+router.get(
+  '/:id',
+  schemaValidator(idSchema),
+  userLogged,
+  ownershipValidator,
+  getAllUsersid.getid
+);
+
+router.put(
+  '/:id',
+  schemaValidator(updateSchema),
+  uploadImage.single('avatar'),
+  userLogged,
+  ownershipValidator,
+  updateUser.run
+);
+
+router.delete(
+  '/:id',
+  schemaValidator(deleteSchema),
+  userLogged,
+  ownershipValidator,
+  deleteUser.run
+);
 
 module.exports = router;
